0c334c8dcea2cac3bfb643a602842ebed9b0a225
Matthew Kim <madkim@ucsc.edu>
2022-04-17 13:07:06 -0700

    Working on projects card carousel.

--- src/_pages/Projects/index.jsx
+++ src/_pages/Projects/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Card, Button, Col, Row } from 'react-bootstrap'
+import { Container, Card, Button, Col, Row, Image } from 'react-bootstrap'
 
 export default function Projects() {
   return (
@@ -9,13 +9,27 @@ export default function Projects() {
         <Col>
           <div id='project__cards' className='d-flex' style={{height: '55vh'}}>
             <div className='project__card'>
-              Project
+              <Image fluid src='https://www.apple.com/newsroom/images/product/app-store/Apple_App-Study-Results_hero.jpg.landing-big_2x.jpg' style={{borderRadius: '1.5em 1.5em 0 0'}} />
+              <div className='project__card--details p-3'>
+                <h5>Project</h5>
+                <p className='lead'>This is the short detail about the project</p>
+              </div>
             </div>
-            <div className='project__card' >
-              Project
+
+            <div className='project__card project__card--active' >
+              <Image fluid src='https://www.apple.com/newsroom/images/product/app-store/Apple_App-Study-Results_hero.jpg.landing-big_2x.jpg' style={{borderRadius: '1.5em 1.5em 0 0'}} />
+              <div className='project__card--details p-3'>
+                <h5>Project</h5>
+                <p className='lead'>This is the short detail about the project</p>
+              </div>
             </div>
+
             <div className='project__card'>
-              Project
+              <Image fluid src='https://www.apple.com/newsroom/images/product/app-store/Apple_App-Study-Results_hero.jpg.landing-big_2x.jpg' style={{borderRadius: '1.5em 1.5em 0 0'}} />
+              <div className='project__card--details p-3'>
+                <h5>Project</h5>
+                <p className='lead'>This is the short detail about the project</p>
+              </div>
             </div>
           </div>
         </Col>
