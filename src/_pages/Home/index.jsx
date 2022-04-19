2bb66c3fa1bc6bb6fc2d14966516b8dfb26b3adb
Matthew Kim <mtthwkm@gmail.com>
2022-04-19 15:08:14 -0700

    Working on home page detail section.

--- src/_pages/Home/index.jsx
+++ src/_pages/Home/index.jsx
@@ -65,11 +65,28 @@ export default function Home() {
       <Container>
         <div id='home__details'>
           <div ref={homeDetailsRef} className='text-center'>
-            <Image height={500} width={500} src={JTreeImg} roundedCircle style={{objectFit: 'cover'}}/>
-            <div className='lead mt-5'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id venenatis sapien. Praesent placerat mauris vel nunc iaculis lobortis. Donec vitae aliquam ipsum. Sed placerat consequat risus at posuere. Donec aliquet molestie euismod. Ut justo risus, pulvinar posuere scelerisque ut, aliquam nec purus. Praesent efficitur arcu lacus, in hendrerit nisl maximus sed. Sed nec metus at nulla viverra tincidunt eget quis ex. Vivamus feugiat lobortis mi id viverra. Aliquam euismod nisi ut ligula venenatis hendrerit. Etiam libero quam, sollicitudin ac viverra eu, auctor at justo. Vestibulum luctus consequat nisi vel dapibus. Vivamus id est pharetra, tincidunt nunc id, eleifend sapien.
-              Aenean placerat, urna sit amet gravida semper, lectus lectus vestibulum felis, at euismod mauris magna nec sapien. Mauris sit amet magna eget arcu ornare vehicula non sit amet lorem. Morbi velit nunc, tincidunt in convallis sed, tempor non nibh. In convallis leo quis nisl porttitor varius. Vivamus eu orci pulvinar, congue erat et, vehicula nulla. Aliquam sollicitudin dolor ac eros faucibus, pellentesque maximus mi tempor. Morbi sollicitudin semper massa, sed vestibulum magna cursus ut. In quam neque, eleifend non suscipit vel, posuere vel quam. Proin ultricies vehicula diam.
-              Donec placerat nisl et vestibulum ullamcorper. Morbi in lacus a arcu efficitur consequat id a ante. Duis sed luctus nunc. Phasellus eu sem iaculis mauris venenatis ultricies id eget magna. Aliquam vitae turpis ac arcu mollis aliquam ut nec enim. Curabitur congue orci lacus, ut molestie augue ornare id. Nam id neque vitae quam euismod iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas rutrum, neque vitae efficitur consectetur, massa diam ultricies ligula, ac dapibus ante leo quis massa. Nullam vitae hendrerit sem. Donec accumsan ullamcorper fringilla. Ut diam nibh, auctor iaculis semper eu, ornare eu nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a dictum nisl, quis gravida diam. Vivamus semper urna ut nulla finibus tempor.
+            <div className='mt-5'>
+              <Image height={300} width={'100%'} src={JTreeImg}   style={{objectFit: 'cover'}}/>
+              <div className=' m-5  d-flex justify-content-center flex-row'>
+                <div className='text-center'>
+                  <p>Hello! I'm Matt <br />  Welcome to my website</p>
+                  <div>
+                    <p>I'm
+                        <br />
+                        a climber 
+                        <br /> 
+                        a music maker 
+                        <br /> 
+                        a Front End developer
+                        <br /> 
+                        an outdoor enthusiast 
+                        <br /> 
+                        a lover of all things coffee </p>
+                  </div>
+                  <p>Please feel free to look around and enjoy your stay</p> 
+                  <p>I'm happy you're here</p>
+                </div>
+            </div>
             </div>
           </div>
         </div>
