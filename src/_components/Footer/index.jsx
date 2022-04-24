1bb17c113439385dcc47c3b45a2e0159eb9d8c2b
Matthew Kim <mtthwkm@gmail.com>
2022-04-24 12:37:02 -0700

    Formatted entire project w/ prettier.

--- src/_components/Footer/index.jsx
+++ src/_components/Footer/index.jsx
@@ -1,18 +1,22 @@
 import React, { useRef, useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom'
 
 export default function Footer() {
   const footerRef = useRef(null)
   const location = useLocation()
-  
+
   useEffect(() => {
     if (location.pathname === '/home') {
-      footerRef.current.style.opacity = 0;
+      footerRef.current.style.opacity = 0
     }
   }, [location.pathname])
 
   return (
-    <div ref={footerRef} id='footer' className='py-1 text-light text-center w-100 lead user-select-none'>
+    <div
+      ref={footerRef}
+      id='footer'
+      className='py-1 text-light text-center w-100 lead user-select-none'
+    >
       mtthwkm.io
     </div>
   )
--- src/_helpers/animation.js
+++ src/_helpers/animation.js
@@ -1,104 +1,104 @@
-import gsap from "gsap";
+import gsap from 'gsap'
 
-    // import ScrollTrigger from 'gsap/ScrollTrigger'
+// import ScrollTrigger from 'gsap/ScrollTrigger'
 
-    // gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(ScrollTrigger)
 
-    // gsap.to("#home__details--container", {
-    //   scrollTrigger: ".home__details", 
-    //   ease: 'linear',
-    //   opacity: 0
-    // });
+// gsap.to("#home__details--container", {
+//   scrollTrigger: ".home__details",
+//   ease: 'linear',
+//   opacity: 0
+// });
 
 // Home Animations
 export function animateHomePage() {
-  gsap.to("#home", {
-    ease: "power",
+  gsap.to('#home', {
+    ease: 'power',
     duration: 1.5,
     opacity: 1
-  });
-  gsap.to(".navbar", {
+  })
+  gsap.to('.navbar', {
     opacity: 1,
-    ease: "power",
+    ease: 'power',
     delay: 2,
-    duration: 1,
-  });
-  gsap.to("#footer", {
+    duration: 1
+  })
+  gsap.to('#footer', {
     opacity: 1,
-    ease: "power",
+    ease: 'power',
     delay: 2,
-    duration: 1,
-  });
-  gsap.to(".home__scroll", {
+    duration: 1
+  })
+  gsap.to('.home__scroll', {
     y: 40,
     opacity: 1,
-    ease: "power",
+    ease: 'power',
     delay: 2,
-    duration: 1,
-  });
-  gsap.to(".home__scroll", {
+    duration: 1
+  })
+  gsap.to('.home__scroll', {
     y: 20,
-    ease: "linear",
+    ease: 'linear',
     delay: 3,
     duration: 1,
     yoyo: true,
-    repeat: -1,
-  });
+    repeat: -1
+  })
 }
 
 // About Animations
 export function animateAboutPage() {
-  gsap.to(".about__choice", {
-    ease: "elastic",
+  gsap.to('.about__choice', {
+    ease: 'elastic',
     opacity: 1,
     delay: 1,
     duration: 2,
     y: -20
-  });
-  gsap.to(".about__item", {
-    ease: "elastic",
+  })
+  gsap.to('.about__item', {
+    ease: 'elastic',
     opacity: 1,
     delay: 2,
     duration: 2,
     y: 20
-  });
+  })
 }
 
 // Projects Animations
 export function animateProjectsPage() {
-  gsap.to("#project__cards", {
-    ease: "linear",
+  gsap.to('#project__cards', {
+    ease: 'linear',
     opacity: 1,
-    duration: .2,
-  });
-  gsap.to(".project__click__me", {
+    duration: 0.2
+  })
+  gsap.to('.project__click__me', {
     y: -10,
     opacity: 1,
-    ease: "power",
+    ease: 'power',
     delay: 2,
-    duration: 1,
-  });
-  gsap.to(".project__click__me", {
+    duration: 1
+  })
+  gsap.to('.project__click__me', {
     y: 10,
-    ease: "linear",
+    ease: 'linear',
     delay: 3,
     duration: 1,
     yoyo: true,
-    repeat: -1,
-  });
+    repeat: -1
+  })
 }
 
 // Contact Animations
 export function animateContactPage() {
-  gsap.to("#contact", {
-    ease: "linear",
+  gsap.to('#contact', {
+    ease: 'linear',
     opacity: 1,
-    duration: 2,
-  });
-  gsap.to(".contact__form", {
-    ease: "linear",
+    duration: 2
+  })
+  gsap.to('.contact__form', {
+    ease: 'linear',
     delay: 2,
     opacity: 1,
-    duration: 1,
-  });
+    duration: 1
+  })
 }
