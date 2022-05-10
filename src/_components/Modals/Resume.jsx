7217e9e1e0a56822c364a21f27a1074db5ec6dcb
Matthew Kim <mtthwkm@gmail.com>
2022-05-10 10:12:35 -0700

    Working on ResumeModal.

--- src/_components/Modals/Resume.jsx
+++ src/_components/Modals/Resume.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import React, { useState } from 'react'
+import ResumePDF from '../../_assets/Resume.pdf'
+
+import { pdfjs } from 'react-pdf';
+import { Document, Page } from "react-pdf";
+import { Button, Modal, Spinner } from 'react-bootstrap'
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 export default function ResumeModal({ show, handleClose }) {
+  const [loading, setLoading] = useState(true)
+
+  const onDocumentLoadSuccess = () => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 500);
+  }
+
   return (
     <Modal size='xl' fullscreen centered show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Resume</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Body className='d-flex flex-column justify-content-center align-items-center container'>
+        <Document file={ResumePDF} onLoadSuccess={onDocumentLoadSuccess}>
+          {/* {loading ?  */}
+            <Spinner animation='border'/>
+            {/* :
+            <Page pageNumber={1} />
+          } */}
+        </Document>
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="outline-success" onClick={handleClose}>
         <i className="fal fa-download"></i> &nbsp;Download
