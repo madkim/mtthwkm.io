8cc2ad3758654f2b2e8808d458f59fa45f0218bc
Matthew Kim <mtthwkm@gmail.com>
2022-05-20 16:14:21 -0700

    Changed endpoint to prod. Contact form now working.

--- src/_services/contact.service.js
+++ src/_services/contact.service.js
@@ -1,7 +1,7 @@
 import axiosInstance from '../_helpers/axios-config'
 
 export function sendContactEmail(name, email, details) {
-  return axiosInstance.post('https://0lvqdvjg9a.execute-api.us-east-1.amazonaws.com/test/contact', {
+  return axiosInstance.post('https://0lvqdvjg9a.execute-api.us-east-1.amazonaws.com/prod/contact', {
     email: email,
     subject: name,
     message: details
