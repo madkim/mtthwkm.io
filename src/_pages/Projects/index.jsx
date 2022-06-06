cff144d4605bf4e90a324c1581703688b0c88508
Matthew Kim <mtthwkm@gmail.com>
2022-06-06 14:17:01 -0700

    I think prev is working.

--- src/_pages/Projects/index.jsx
+++ src/_pages/Projects/index.jsx
@@ -20,7 +20,7 @@ export default function Projects() {
   }  
 
   const handlePrev = () => {
-    const prevStep = (currentStep - 4) % 5
+    const prevStep = currentStep === 0 ? 4 : (currentStep - 1) % 5
     console.log(prevStep)
     setCurrentStep(prevStep)
   }
